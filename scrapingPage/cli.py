b14ab655f27c32e0912bcc75e67f39bb18c38547
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2022-05-01 13:11:42 -0300

    Iniciando projeto

---
+++ scrapingPage/cli.py
@@ -0,0 +1,11 @@
+import typer
+from typing import Optional
+from rich.console import Console
+from rich.table import Table
+from rich import print
+
+
+
+
+main = typer.Typer(help="Beer Management Application")
+console = Console()
